97240b809b7689cc33fd3d94e5e32d189d61e63e
kiwiscott <you@example.com>
2020-12-18 12:44:15 -0500

    Day 17 : Don't write what you don't have too

--- src/day17.rs
+++ src/day17.rs
@@ -29,10 +29,7 @@ fn parse_input(input: &str) -> Vec<Cube> {
 }
 #[aoc(day17, part1)]
 fn part1(cubes: &Vec<Cube>) -> i32 {
-    let mut dimension = PocketDimension {
-        cubes: cubes.clone(),
-        dimensions: 3,
-    };
+    let mut dimension = PocketDimension::new(cubes.clone(), 3);
     for _ in 1..=6 {
         dimension.cycle();
     }
@@ -41,10 +38,7 @@ fn part1(cubes: &Vec<Cube>) -> i32 {
 
 #[aoc(day17, part2)]
 fn part2(cubes: &Vec<Cube>) -> i32 {
-    let mut dimension = PocketDimension {
-        cubes: cubes.clone(),
-        dimensions: 4,
-    };
+    let mut dimension = PocketDimension::new(cubes.clone(), 4);
     for _ in 1..=6 {
         dimension.cycle();
     }
@@ -105,59 +99,106 @@ const ACTIVE: bool = true;
 const INACTIVE: bool = false;
 type Cube = (Coordinate, bool);
 struct PocketDimension {
-    cubes: Vec<Cube>,
+    cubes: Vec<Coordinate>,
     dimensions: i32,
+    min: Coordinate,
+    max: Coordinate,
 }
 
 impl PocketDimension {
+    fn new(cubes: Vec<Cube>, dimensions: i32) -> PocketDimension {
+        let min = cubes.iter().min().unwrap().0;
+        let max = cubes.iter().max().unwrap().0;
+        let new_cube: Vec<Coordinate> = cubes
+            .iter()
+            .filter(|(_, act)| *act)
+            .map(|(co, _)| *co)
+            .collect();
+
+        PocketDimension {
+            cubes: new_cube,
+            dimensions: dimensions,
+            min: min,
+            max: max,
+        }
+    }
+
     fn cycle(&mut self) {
-        let min = self.min_cordinate().unwrap();
-        let max = self.max_cordinate().unwrap();
-
         let mut new_cube = vec![];
-
-        for w in self.range_for('w', min, max) {
-            for z in self.range_for('z', min, max) {
-                for y in self.range_for('y', min, max) {
-                    for x in self.range_for('x', min, max) {
+        for w in self.range_for('w') {
+            for z in self.range_for('z') {
+                for y in self.range_for('y') {
+                    for x in self.range_for('x') {
                         let c = match self.active_neighbours(x, y, z, w) {
-                            3 => (Coordinate::new(x, y, z, w), ACTIVE),
-                            2 => (Coordinate::new(x, y, z, w), self.is_active(x, y, z, w)),
-                            _ => (Coordinate::new(x, y, z, w), INACTIVE),
+                            3 => Some(Coordinate::new(x, y, z, w)),
+                            2 => match self.is_active(x, y, z, w) {
+                                true => Some(Coordinate::new(x, y, z, w)),
+                                false => None,
+                            },
+                            _ => None, //(Coordinate::new(x, y, z, w), INACTIVE),
                         };
-                        new_cube.push(c);
+
+                        match c {
+                            Some(t) => new_cube.push(t),
+                            None => (),
+                        }
                     }
                 }
             }
         }
+
+        self.set_new_min_max();
         self.cubes = new_cube;
     }
-    fn range_for(&self, dimension: char, min: Coordinate, max: Coordinate) -> Range<i32> {
+
+    fn set_new_min_max(&mut self) {
+        self.max = Coordinate::new(
+            self.max.x + 1,
+            self.max.y + 1,
+            self.max.z + 1,
+            if self.dimensions <= 3 {
+                self.max.w
+            } else {
+                self.max.w + 1
+            },
+        );
+        self.min = Coordinate::new(
+            self.min.x - 1,
+            self.min.y - 1,
+            self.min.z - 1,
+            if self.dimensions <= 3 {
+                self.min.w
+            } else {
+                self.min.w - 1
+            },
+        );
+    }
+    fn range_for(&self, dimension: char) -> Range<i32> {
         let mut r = Range { start: 0, end: 1 };
 
         match dimension {
             'w' => {
                 if self.dimensions >= 4 {
-                    r.start = min.w - 1;
-                    r.end = max.w + 1 + 1;
+                    r.start = self.min.w - 1;
+                    r.end = self.max.w + 1 + 1;
                 }
             }
             'z' => {
                 if self.dimensions >= 3 {
-                    r.start = min.z - 1;
-                    r.end = max.z + 1 + 1;
+                    r.start = self.min.z - 1;
+                    r.end = self.max.z + 1 + 1;
                 }
             }
             'y' => {
                 if self.dimensions >= 2 {
-                    r.start = min.y - 1;
-                    r.end = max.y + 1 + 1;
+                    r.start = self.min.y - 1;
+                    r.end = self.max.y + 1 + 1;
                 }
             }
             'x' => {
                 if self.dimensions >= 2 {
-                    r.start = min.x - 1;
-                    r.end = max.x + 1 + 1;
+                    r.start = self.min.x - 1;
+                    r.end = self.max.x + 1 + 1;
                 }
             }
             _ => (),
@@ -168,30 +209,17 @@ impl PocketDimension {
     fn is_active(&self, x: i32, y: i32, z: i32, w: i32) -> bool {
         self.cubes
             .iter()
-            .any(|(co, st)| *st && (co.w == w && co.x == x && co.y == y && co.z == z))
+            .any(|co| co.w == w && co.x == x && co.y == y && co.z == z)
     }
     fn active_cubes(&self) -> i32 {
-        self.cubes.iter().filter(|(_, st)| *st).count() as i32
-    }
-
-    fn min_cordinate(&self) -> Option<Coordinate> {
-        match self.cubes.iter().min() {
-            Some(c) => Some(c.0),
-            None => None,
-        }
-    }
-    fn max_cordinate(&self) -> Option<Coordinate> {
-        match self.cubes.iter().max() {
-            Some(c) => Some(c.0),
-            None => None,
-        }
+        self.cubes.iter().count() as i32
     }
 
     fn active_neighbours(&self, x: i32, y: i32, z: i32, w: i32) -> i32 {
         self.cubes
             .iter()
-            .filter(|(co, st)| {
-                *st && (self.dimensions < 1||(co.x >= x - 1 && co.x <= x + 1 ))
+            .filter(|co| {
+                (self.dimensions < 1||(co.x >= x - 1 && co.x <= x + 1 ))
                     && (self.dimensions < 2||(co.y >= y - 1 && co.y <= y + 1 ))
                     && (self.dimensions < 3||(co.z >= z - 1 && co.z <= z + 1 ))
                     && (self.dimensions < 4||(co.w >= w - 1 && co.w <= w + 1 ))
@@ -292,10 +320,7 @@ mod tests {
                 true,
             ),
         ];
-        let dimension = PocketDimension {
-            cubes: parse_input(s),
-            dimensions: 3,
-        };
+        let dimension = PocketDimension::new(parse_input(s), 3);
 
         assert_eq!(dimension.cubes.len(), 9);
         for (ec, es) in expected.iter() {
@@ -316,28 +341,11 @@ mod tests {
             );
         }
     }
-    #[test]
-    fn test_cordinates() {
-        let s = ".#.\n..#\n###";
-        let dimension = PocketDimension {
-            cubes: parse_input(s),
-            dimensions: 3,
-        };
-
-        let min = dimension.min_cordinate().unwrap();
-        assert_eq!((min.x, min.y, min.z), (0, 0, 0));
-
-        let max = dimension.max_cordinate().unwrap();
-        assert_eq!((max.x, max.y, max.z), (2, 2, 0));
-    }
 
     #[test]
     fn neighbours_in_grid() {
         let s = ".#.\n..#\n###";
-        let dimension = PocketDimension {
-            cubes: parse_input(s),
-            dimensions: 3,
-        };
+        let dimension = PocketDimension::new(parse_input(s), 3);
 
         assert_eq!(1, dimension.active_neighbours(0, 0, 0, 0));
         assert_eq!(2, dimension.active_neighbours(2, 2, 0, 0));
@@ -345,10 +353,7 @@ mod tests {
     #[test]
     fn neignbours_different_z() {
         let s = ".#.\n..#\n###";
-        let dimension = PocketDimension {
-            cubes: parse_input(s),
-            dimensions: 3,
-        };
+        let dimension = PocketDimension::new(parse_input(s), 3);
 
         assert_eq!(1, dimension.active_neighbours(0, 0, -1, 0));
         assert_eq!(2, dimension.active_neighbours(3, 2, 1, 0));
@@ -357,10 +362,7 @@ mod tests {
     #[test]
     fn sample_test() {
         let s = ".#.\n..#\n###";
-        let mut dimension = PocketDimension {
-            cubes: parse_input(s),
-            dimensions: 3,
-        };
+        let mut dimension = PocketDimension::new(parse_input(s), 3);
 
         for _ in 1..=6 {
             dimension.cycle();
@@ -370,10 +372,7 @@ mod tests {
     #[test]
     fn second_part_test() {
         let s = ".#.\n..#\n###";
-        let mut dimension = PocketDimension {
-            cubes: parse_input(s),
-            dimensions: 4,
-        };
+        let mut dimension = PocketDimension::new(parse_input(s), 4);
 
         for _ in 1..=6 {
             dimension.cycle();
